05630aa6cb623d4e1a92e28d9b427f7c18161675
Hammad Hassan <hammadhassansheikh@gmail.com>
2016-12-30 01:11:43 +0500

    code js code

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 function name1() {
+}
+//Name
 var name = document.getElementById("name").value;
 if (name === "" || name === "  " || name === " ") {
     alert("Please Enter Name");
 } else {
     name;
+    //name.value= "";
+    };
+//Email
+var mail = document.getElementById("mail").value;
+if (mail === "" || mail === "  " || mail === " ") {
+    alert("Please Enter Email");
+} else {
+    mail;
+    var newmail = mail;
+    //mail.value= "";
+    };
+ //Password
+ var passw = document.getElementById("pass").value;
+if (passw === "" || passw === "  " || passw === " ") {
+    alert("Please Enter Password");
+} else {
+    passw;
+    var newpass = pass;
+    //passw.value= "";
+    };   
+//Signup 
+function signup() {
+    var name1 = name;
+    var mail1 = mail;
+    var pass1 = passw;
+    alert("submit");
+    mail.value = "";
+    pass.value = "";
+    name.value = "";
+}
+//Login
+function login() {
+    if (newmail === mail && newpass === pass) {
+     alert("Success");
+    }
+    else {
+    alert("Please Enter Correct info")
     }
 }
