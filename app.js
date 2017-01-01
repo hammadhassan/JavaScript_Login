d8b754ecedd91363dc1d3d4e77a6fa127ef080f7
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-01-01 23:40:19 +0500

    attentionate

--- app.js
+++ app.js
@@ -26,43 +26,11 @@ function login() {
     }
     var data = JSON.parse(localStorage.getItem("user"))
     for (i = 0; i < data.length; i++) {
-        if (obj.email == data[i].email && obj.pass == data[i].pass) {
+        if (obj.email == data[i].mail && obj.pass == data[i].pass) {
             alert("Login")
         } else {
-            console.log("Please Check email or Password")
+            alert("Please Check email or Password")
         }
         //console.log(data[i].email)
     }
 }
-///Name
-//     if (Name === "" || Name === "  " || Name === " ") {
-//         alert("Please Enter Name");
-//     } else {
-//         var name1 = Name;
-//         name.value= "";
-//         }
-// //Email
-//     if (Mail === "" || Mail === "  " || Mail === " ") {
-//         alert("Please Enter Email");
-//     } else {
-//         var mail1 = Mail;
-//         mail.value = "";
-//         }
-// //Password
-//     if (Pass === "" || Pass === "  " || Pass === " ") {
-//         alert("Please Enter Password");
-//     } else {
-//         var pass1 = Pass;
-//         passw.value = "";
-//     }
-//     alert("submit");
-// }
-// //Login
-// function login() {
-//     if (document.getElementById("mail1").value === mail && document.getElementById("pass1").value === passw) {
-//      alert("Success");
-//     }
-//     else {
-//     alert("Please Enter Correct info")
-//     }
-// }
